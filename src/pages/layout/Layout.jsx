2894337e403371e35ec951aab87d1c2727b71e91
JamshidPardayev <jamshidnt2024@gmail.com>
2025-05-23 02:25:19 +0500

    new

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import { Outlet } from 'react-router-dom'
 import Hero from '../../components/hero/Hero'
-import Footer from '../../components/footer/Footer'
+import Footer from "../../components/footer/Footer";
+
 const Layout = () => {
   return (
     <div>
