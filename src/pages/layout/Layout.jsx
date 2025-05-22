cb76473d73c5eb811c5cac5a97775d3c68e64ab9
JamshidPardayev <jamshidnt2024@gmail.com>
2025-05-23 02:31:36 +0500

    footer tugirlandi

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import { Outlet } from 'react-router-dom'
 import Hero from '../../components/hero/Hero'
+import Footer from '../../components/footer/Footer'
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <Header />
       <Hero />
       <Outlet />
+      <Footer />
     </div>
   )
 }
